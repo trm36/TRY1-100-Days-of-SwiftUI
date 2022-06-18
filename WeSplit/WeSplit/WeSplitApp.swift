dab264d7ef14d983fd428dad1563aca580f35601
Taylor Mott <trm36@humboldt.edu>
2022-06-17 19:57:33 -0600

    Creates WeSplit Project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Taylor on 17 June 2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
