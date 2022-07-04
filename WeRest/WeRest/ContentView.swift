c21331f99912b8e0cc5c8efb2454897eaa43f610
Taylor Mott <trm36@humboldt.edu>
2022-07-04 15:22:15 -0600

    DAY-26 Stepper

--- WeRest/WeRest/ContentView.swift
+++ WeRest/WeRest/ContentView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
         }
+        .padding()
     }
 }
 
