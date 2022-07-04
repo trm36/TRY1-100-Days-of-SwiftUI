b0a3b686db322b64f820ef91e37adc9da363a00f
Taylor Mott <trm36@humboldt.edu>
2022-07-04 15:18:51 -0600

    DAY-26 WeRest project setup

---
+++ WeRest/WeRest/WeRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeRestApp.swift
+//  WeRest
+//
+//  Created by Taylor on 04 July 2022.
+//
+
+import SwiftUI
+
+@main
+struct WeRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
