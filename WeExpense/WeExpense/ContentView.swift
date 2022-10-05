aba5035c999684bd2b68ecb5132bec874f1417c3
Taylor Mott <trm36@humboldt.edu>
2022-10-04 21:33:18 -0600

    DAY-36 WeExpense Initial Commit

---
+++ WeExpense/WeExpense/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeExpense
+//
+//  Created by Taylor on 04 October 2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeExpense/WeExpense/WeExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeExpenseApp.swift
+//  WeExpense
+//
+//  Created by Taylor on 04 October 2022.
+//
+
+import SwiftUI
+
+@main
+struct WeExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
