88d9188cc9a3c8a4a62c229865c61a515f9aa876
Taylor Mott <trm36@humboldt.edu>
2022-10-08 21:06:43 -0600

    DAY-39 WeMoon Initial Commit

---
+++ WeMoon/WeMoon/WeMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeMoonApp.swift
+//  WeMoon
+//
+//  Created by Taylor on 08 October 2022.
+//
+
+import SwiftUI
+
+@main
+struct WeMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
