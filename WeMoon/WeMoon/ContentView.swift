d7dd70ccecee149512a6e76300bf0048db371253
Taylor Mott <trm36@humboldt.edu>
2022-10-08 23:19:07 -0600

    DAY-39 ScrollView

--- WeMoon/WeMoon/ContentView.swift
+++ WeMoon/WeMoon/ContentView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("Example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
+//        GeometryReader { geo in
+//            Image("Example")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: geo.size.width * 0.8)
+//                .frame(width: geo.size.width, height: geo.size.height)
+//        }
+        
+        
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
@@ -22,5 +33,18 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomText: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText \(text)")
+        self.text = text
     }
 }
