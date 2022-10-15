ab6ec7b0a0e9b18b21e02bc952374a9d3378dcd1
Taylor Mott <trm36@humboldt.edu>
2022-10-15 12:38:38 -0600

    DAY-43 WeDraw Initial Commit

---
+++ WeDraw/WeDraw/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeDraw
+//
+//  Created by Taylor on 15 October 2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeDraw/WeDraw/WeDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeDrawApp.swift
+//  WeDraw
+//
+//  Created by Taylor on 15 October 2022.
+//
+
+import SwiftUI
+
+@main
+struct WeDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
